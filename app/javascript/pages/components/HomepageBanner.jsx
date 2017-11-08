8b8f08887bd2b2671f074ad7ce3cd56f35faeb3c
jordan gomes <jordasg@gmail.com>
2017-11-08 23:08:51 +0200

    finished presentational part of homepage (kinda). needs some more backend logic to be 100%.

--- app/javascript/pages/components/HomepageBanner.jsx
+++ app/javascript/pages/components/HomepageBanner.jsx
@@ -3,26 +3,29 @@ import styled from "styled-components";
 import { styles } from "../../style-variables";
 
 const Wrapper = styled.div`
+  padding-top: 50px;
+  opacity: .99;
   overflow-x: hidden;
   background-image: url("https://images.unsplash.com/photo-1498837167922-ddd27525d352?dpr=1&auto=format&fit=crop&w=1950&h=&q=60&cs=tinysrgb&crop=");
   background-position: center;
   background-attachment: fixed;
   background-size: cover;
-  z-index: 10;
-  min-height: calc(100vh - var(--navbar-height) - 10vh);
+  position: static;
+  z-index: -10;
+  min-height: calc(100vh);
   &::after {
+    opacity: .99;
     overflow-x: hidden;
     content: "";
     display: block;
     position: absolute;
     top: 0;
     left: -1250px;
-
     background: linear-gradient(90deg, #2bc0e4 50%, #eaecc6aa);
     width: 2500px;
     height: 2500px;
     transform: rotate(45deg);
-    z-index: 3;
+    z-index: -6;
   }
   &::before {
     content: "";
@@ -36,9 +39,127 @@ const Wrapper = styled.div`
     transform: rotate(45deg);
     z-index: -9;
     box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.2);
-    z-index: 5;
+    z-index: -5;
 `;
 
-const HomepageBanner = () => <Wrapper />;
+const ContentWrapper = styled.div`
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+`;
+
+const Header = styled.h1`
+  font-family: ${styles.fonts.raleway};
+  font-weight: 900;
+  color: white;
+  font-size: 6rem;
+  max-width: 30vw;
+  line-height: 1.5;
+  letter-spacing: 4px;
+  margin-bottom: 0;
+`;
+
+const Subheader = styled.h3`
+  font-weight: 300;
+  font-size: 3em;
+  margin-top: 30px;
+  color: ${styles.colors.brightGreen};
+  font-family: "Open Sans Condensed";
+  letter-spacing: 18px;
+  margin-bottom: 8rem;
+`;
+
+const SearchBarWrapper = styled.div`
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  width: 1200px;
+  max-width: 1200px;
+  box-shadow: 0 0 16px 2px rgba(0, 0, 0, 0.12);
+  margin: 0 auto;
+  padding: 5px;
+  transition-duration: 0.2s;
+  &.focus {
+    background-color: rgba(43, 192, 228, 0.8);
+    box-shadow: 0 0 16px 4px rgba(0, 0, 0, 0.16);
+  }
+`;
+
+const SearchBar = styled.input`
+  font-family: ${styles.fonts.montserrat};
+  max-width: 1100px;
+  flex: 1;
+  border: none;
+  font-size: 2.125rem;
+  border-radius: 5px 0 0 5px;
+  padding: 5px;
+  // text-align: center;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SearchButton = styled.div`
+  background-color: ${styles.colors.mainBlue};
+  color: white;
+  // flex: 1;
+  padding: 0 25px;
+  height: 100%;
+  font-family: ${styles.fonts.lato};
+  text-align: center;
+  font-size: 2.125rem;
+  vertical-align: bottom;
+  height: calc(2.125rem + 15px);
+  border-radius: 3px;
+  cursor: pointer;
+  transition-duration: 0.2s;
+  &:hover {
+    background-color: #2bc0e4;
+  }
+  p {
+    margin: 0;
+    vertical-align: bottom;
+    margin-top: 3px;
+  }
+`;
+
+class HomepageBanner extends Component {
+  state = {
+    searchBarFocus: false
+  };
+
+  handleSearchbarFocus() {
+    this.setState({ searchBarFocus: true });
+  }
+
+  handleSearchbarBlur() {
+    this.setState({ searchBarFocus: false });
+  }
+
+  render() {
+    const searchBarFocus = this.state.searchBarFocus ? "focus" : "";
+    return (
+      <Wrapper>
+        <ContentWrapper>
+          <Header>YOUR FAVORITE RESTAURANTS</Header>
+          <Subheader>NOW WITH DELIVERY.</Subheader>
+          <SearchBarWrapper className={searchBarFocus}>
+            <SearchBar
+              placeholder="🔍  Start by typing your street name or P.O. Box"
+              onFocus={this.handleSearchbarFocus.bind(this)}
+              onBlur={this.handleSearchbarBlur.bind(this)}
+            />
+            <SearchButton>
+              <p>Go</p>
+            </SearchButton>
+          </SearchBarWrapper>
+        </ContentWrapper>
+      </Wrapper>
+    );
+  }
+}
 
 export default HomepageBanner;
--- app/javascript/style-variables.js
+++ app/javascript/style-variables.js
@@ -3,7 +3,8 @@ export const styles = {
     mainBlue: "#3297D3",
     grey: "#4f4f4f",
     lightGrey: "#828282",
-    lighterGrey: "#949494"
+    lighterGrey: "#949494",
+    brightGreen: "#c6ff00"
   },
   fonts: {
     lato:
@@ -11,6 +12,8 @@ export const styles = {
     montserrat:
       "'Montserrat', -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif !important;",
     nunito:
-      "'Nunito', -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif !important;"
+      "'Nunito', -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif !important;",
+    raleway:
+      "'Raleway', -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif !important;"
   }
 };
